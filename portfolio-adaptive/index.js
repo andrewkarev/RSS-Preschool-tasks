bf8ad723e89b3e48be25f86993c299053aecac5f
andrewkarev <an.carev4@gmail.com>
2022-01-03 15:28:36 +0300

    feat: add burger menu hover effect

--- portfolio-adaptive/index.js
+++ portfolio-adaptive/index.js
@@ -2,6 +2,9 @@ const body = document.getElementById('body');
 const hamburger = document.querySelector('.hamburger');
 const headerNav = document.querySelector('.header-navigation');
 const navMenu = document.querySelector('.navigation-list')
+const line1 = document.querySelector('.line1')
+const line2 = document.querySelector('.line2')
+const line3 = document.querySelector('.line3')
 
 
 function toggleMenu() {
@@ -18,5 +21,22 @@ function closeMenu(event) {
   }
 }
 
+function hoverButton() {
+  line1.classList.toggle('active');
+  line2.classList.toggle('active');
+  line3.classList.toggle('active');
+}
+
+function stopHovering() {
+  line1.classList.remove('active');
+  line2.classList.remove('active');
+  line3.classList.remove('active');
+}
+
+
+hamburger.addEventListener('mouseover', hoverButton);
+hamburger.addEventListener('mouseout', stopHovering);
 hamburger.addEventListener('click', toggleMenu);
 navMenu.addEventListener("click", closeMenu);
+
+
