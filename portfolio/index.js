2f8114d269fdc5456206ed56bf5c00883e262fda
andrewkarev <an.carev4@gmail.com>
2022-01-05 20:10:51 +0300

    feat: add self-evaluation

---
+++ portfolio/index.js
@@ -0,0 +1,47 @@
+console.log(`
+Rolling Scopes School
+Stage 0 Portfolio #1 task
+Self-evaluation by Andrew Karev
+
+✓ The markup is valid - 10 points
+
+✓ The markup is semantic - 20 pointts
+    [+] header, main, footer - +2
+    [+] six sections - +2
+    [+] only one h1 - +2
+    [+] five h2 titles - +2
+    [+] only one nav - +2
+    [+] two ul>li lists - +2
+    [+] ten buttons - +2
+    [+] two inputs - +2
+    [+] one textarea - +2
+    [+] three placrholder atributes - +2
+
+✓ The markup matches template - 48 pointts
+    [+] header block - +6
+    [+] hero section - +6
+    [+] skills section - +6
+    [+] portfolio section - +6
+    [+] video section - +6
+    [+] price section - +6
+    [+] contacts section - +6
+    [+] footer block - +6
+
+✓ Css requirements - 12 pointts
+    [+] was used flex-layout - +2
+    [+] the markup centered when zoom out - +2
+    [+] the background color stretches to the full width of the page - +2
+    [+] icons were added in .svg format - +2
+    [+] images were added in .jpg format - +2
+    [+] was added favicon - +2
+
+✓ Interactivity realized with css - 20 pointts
+    [+] smooth scrolling - +5
+    [+] links to the authors github and courses page in the footer block - +5
+    [+] changing in background color and font color on hover - +5
+    [+] smooth change in the appearance of an element on hover and click, which doesn't affect adjacent elements - +5
+
+Total score: 110/100 points
+
+developed by andrewkarev - 2022
+`);
