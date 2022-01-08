1ef774cbb4bc12847c5b35ac0627c41321110940
andrewkarev <an.carev4@gmail.com>
2022-01-08 10:28:31 +0300

    refactor: formatting portfolio#2 code

--- portfolio-adaptive/index.js
+++ portfolio-adaptive/index.js
@@ -1,4 +1,4 @@
-const body = document.getElementById('body');
+const body = document.querySelector('#body');
 const hamburger = document.querySelector('.hamburger');
 const headerNav = document.querySelector('.header-navigation');
 const navMenu = document.querySelector('.navigation-list')
@@ -8,29 +8,29 @@ const line3 = document.querySelector('.line3')
 
 
 function toggleMenu() {
-  hamburger.classList.toggle('open');
-  headerNav.classList.toggle('open');
-  body.classList.toggle("lock");
+  hamburger.classList.toggle('-open');
+  headerNav.classList.toggle('-open');
+  body.classList.toggle("-lock");
 }
 
 function closeMenu(event) {
   if (event.target.classList.contains('nav-link')) {
-    hamburger.classList.remove('open');
-    headerNav.classList.remove('open');
-    body.classList.remove("lock");
+    hamburger.classList.remove('-open');
+    headerNav.classList.remove('-open');
+    body.classList.remove("-lock");
   }
 }
 
 function hoverButton() {
-  line1.classList.toggle('active');
-  line2.classList.toggle('active');
-  line3.classList.toggle('active');
+  line1.classList.toggle('-active');
+  line2.classList.toggle('-active');
+  line3.classList.toggle('-active');
 }
 
 function stopHovering() {
-  line1.classList.remove('active');
-  line2.classList.remove('active');
-  line3.classList.remove('active');
+  line1.classList.remove('-active');
+  line2.classList.remove('-active');
+  line3.classList.remove('-active');
 }
 
 
