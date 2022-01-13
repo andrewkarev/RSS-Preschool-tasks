828d3ec408aa5cde55b127fd9f5bf92c99bd2bff
andrewkarev <an.carev4@gmail.com>
2022-01-13 22:03:21 +0300

    feat: add progress tracking to range type elements

--- js30#1.3-custom-video/index.js
+++ js30#1.3-custom-video/index.js
@@ -1,1 +1,16 @@
-// START VIDEO PLAYER PROJECT
+const progress = document.querySelector('.progress');
+const volume = document.querySelector('.volume');
+const speed = document.querySelector('.speed');
+
+function trackProgress() {
+  const value = this.value;
+  const maxValue = this.max;
+  const minValue = this.min;
+  const percent = Math.round(((value - minValue) / (maxValue - minValue)) * 100)
+
+  this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${percent}%, #fff ${percent}%, #fff 100%)`
+}
+
+progress.addEventListener('input', trackProgress)
+volume.addEventListener('input', trackProgress)
+speed.addEventListener('input', trackProgress)
