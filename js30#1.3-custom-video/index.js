7a7523afa4bc2c249b71c73df6a4128bbe656e96
andrewkarev <an.carev4@gmail.com>
2022-01-14 12:16:49 +0300

    feat: add play function

--- js30#1.3-custom-video/index.js
+++ js30#1.3-custom-video/index.js
@@ -1,16 +1,48 @@
-const progress = document.querySelector('.progress');
-const volume = document.querySelector('.volume');
-const speed = document.querySelector('.speed');
+const player = document.querySelector('.video-player')
+const video = player.querySelector('.viewer')
+const previewBtn = player.querySelector('.video-player-preview-btn')
+// const progressDiv = player.querySelector('.video-player-progress')
+const play = player.querySelector('.play-icon');
+
+const progress = player.querySelector('.progress');
+const volume = player.querySelector('.volume');
+const speed = player.querySelector('.speed');
 
 function trackProgress() {
   const value = this.value;
   const maxValue = this.max;
   const minValue = this.min;
-  const percent = Math.round(((value - minValue) / (maxValue - minValue)) * 100)
+  const percent = Math.round(((value - minValue) / (maxValue - minValue)) * 100);
 
-  this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${percent}%, #fff ${percent}%, #fff 100%)`
+  this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${percent}%, #fff ${percent}%, #fff 100%)`;
 }
 
-progress.addEventListener('input', trackProgress)
-volume.addEventListener('input', trackProgress)
-speed.addEventListener('input', trackProgress)
+progress.addEventListener('input', trackProgress);
+volume.addEventListener('input', trackProgress);
+speed.addEventListener('input', trackProgress);
+
+
+function togglePlay() {
+  if (video.paused) {
+    previewBtn.style.display = 'none'
+    video.play()
+  } else {
+    previewBtn.style.display = 'block'
+    video.pause()
+  }
+}
+
+function updatePlayButton() {
+  console.log(this)
+  if (this.paused) {
+    play.classList.add('pause')
+  } else {
+    play.classList.remove('pause')
+  }
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+previewBtn.addEventListener('click', togglePlay);
+play.addEventListener('click', togglePlay);
