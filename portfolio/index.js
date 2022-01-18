a5e396f5997d32393611daa01667346ce23eea80
andrewkarev <an.carev4@gmail.com>
2022-01-18 15:10:15 +0300

    refactor: copy adaptive version from pf#1 branch

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,47 +1,43 @@
-console.log(`
-Rolling Scopes School
-Stage 0 Portfolio #1 task
-Self-evaluation by Andrew Karev
+import selfEvaluation from './selfeval.js'
+const body = document.querySelector('#body');
+const hamburger = document.querySelector('.hamburger');
+const headerNav = document.querySelector('.header-navigation');
+const navMenu = document.querySelector('.navigation-list')
+const line1 = document.querySelector('.line1')
+const line2 = document.querySelector('.line2')
+const line3 = document.querySelector('.line3')
 
-✓ The markup is valid - 10 points
 
-✓ The markup is semantic - 20 pointts
-    [+] header, main, footer - +2
-    [+] six sections - +2
-    [+] only one h1 - +2
-    [+] five h2 titles - +2
-    [+] only one nav - +2
-    [+] two ul>li lists - +2
-    [+] ten buttons - +2
-    [+] two inputs - +2
-    [+] one textarea - +2
-    [+] three placrholder atributes - +2
+function toggleMenu() {
+  hamburger.classList.toggle('-open');
+  headerNav.classList.toggle('-open');
+  body.classList.toggle("-lock");
+}
 
-✓ The markup matches template - 48 pointts
-    [+] header block - +6
-    [+] hero section - +6
-    [+] skills section - +6
-    [+] portfolio section - +6
-    [+] video section - +6
-    [+] price section - +6
-    [+] contacts section - +6
-    [+] footer block - +6
+function closeMenu(event) {
+  if (event.target.classList.contains('nav-link')) {
+    hamburger.classList.remove('-open');
+    headerNav.classList.remove('-open');
+    body.classList.remove("-lock");
+  }
+}
 
-✓ Css requirements - 12 pointts
-    [+] was used flex-layout - +2
-    [+] the markup centered when zoom out - +2
-    [+] the background color stretches to the full width of the page - +2
-    [+] icons were added in .svg format - +2
-    [+] images were added in .jpg format - +2
-    [+] was added favicon - +2
+function hoverButton() {
+  line1.classList.toggle('-active');
+  line2.classList.toggle('-active');
+  line3.classList.toggle('-active');
+}
 
-✓ Interactivity realized with css - 20 pointts
-    [+] smooth scrolling - +5
-    [+] links to the authors github and courses page in the footer block - +5
-    [+] changing in background color and font color on hover - +5
-    [+] smooth change in the appearance of an element on hover and click, which doesn't affect adjacent elements - +5
+function stopHovering() {
+  line1.classList.remove('-active');
+  line2.classList.remove('-active');
+  line3.classList.remove('-active');
+}
 
-Total score: 110/100 points
 
-developed by andrewkarev - 2022
-`);
+hamburger.addEventListener('mouseover', hoverButton);
+hamburger.addEventListener('mouseout', stopHovering);
+hamburger.addEventListener('click', toggleMenu);
+navMenu.addEventListener("click", closeMenu);
+
+console.log(selfEvaluation)
