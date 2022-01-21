5991f5b35ccb6d385183be3e2d6f6079e4487b20
andrewkarev <an.carev4@gmail.com>
2022-01-21 10:57:18 +0300

    feat: add card rotation on click

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,1 +1,9 @@
 // Scrypt file
+const cards = document.querySelectorAll('.game__card');
+
+function invertCard() {
+  this.classList.toggle('invert');
+  console.log(this)
+}
+
+cards.forEach(card => card.addEventListener('click', invertCard));
