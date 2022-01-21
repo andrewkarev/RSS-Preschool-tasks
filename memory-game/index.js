1670126c97e4f18bda5203fed7fdf5706478583a
andrewkarev <an.carev4@gmail.com>
2022-01-21 17:35:27 +0300

    feat: add card spin logic

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,10 +1,39 @@
 // Scrypt file
 const cards = document.querySelectorAll('.game__card');
 
-function spinCard() {
-  this.classList.add('spin');
+let cardIsSpinned = false;
+let isLoced = false;
+let firstCard, secondCard;
 
-  setTimeout(() => this.classList.remove('spin'), 1500)
+function spinCard() {
+  if (isLoced) {
+    return;
+  }
+
+  if (!cardIsSpinned) {
+    this.classList.add('spin');
+    firstCard = this;
+    cardIsSpinned = true;
+  } else if (this !== firstCard) {
+    this.classList.add('spin');
+    secondCard = this;
+
+    if (firstCard.dataset.character === secondCard.dataset.character) {
+      cardIsSpinned = false;
+      firstCard.removeEventListener('click', spinCard);
+      secondCard.removeEventListener('click', spinCard);
+    } else {
+      isLoced = true;
+      setTimeout(() => {
+        firstCard.classList.remove('spin');
+        secondCard.classList.remove('spin');
+
+        cardIsSpinned = false;
+        isLoced = false;
+      }, 1500);
+    }
+  }
+
 }
 
 cards.forEach(card => card.addEventListener('click', spinCard));
