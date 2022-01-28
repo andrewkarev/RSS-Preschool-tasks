4dbd3db6cabdc66e7a0c8942a7896d60f337f1ba
andrewkarev <an.carev4@gmail.com>
2022-01-28 10:17:39 +0300

    feat: add self-evaluation

--- portfolio/selfeval.js
+++ portfolio/selfeval.js
@@ -1,11 +1,26 @@
 const selfEvaluation = `
-Rolling Scopes School - Stage 0 Portfolio task
+Thanks for taking the time to check my work!
+
+Rolling Scopes School
+Stage 0 Portfolio #3 task
+Self-evaluation by Andrew Karev
 
 Self-evaluation by Andrew Karev
-✓ The markup is valid - 10 points
-✓
+✓ Image changing in portfolio section - 25 points
+    [+] clicking on 'Winter', 'Spring', 'Summer', 'Autumn' buttons changes images to the relevant - +20
+    [+] button which was clicked became active and all remaining buttons became non-active - +5
+✓ Ability to change web-page language - 25 points
+    [+] clicking on 'ru' button, change web-page language to russian - +10
+    [+] clicking on 'en' button, change web-page language to english - +10
+    [+] buttons 'en', 'ru' became active and highlighted with appropriate style - +5
+✓ Ability to change web-page theme - 25 points
+    [+] dark theme replaces light theme - +10
+    [+] light theme replaces dark theme - +10
+    [+] after theme changing all interactive elements workes correctly - +5
+✓ User settings are stored in local storage - 5 points
+✓ Implemented ripple effect for buttons in 'hero', 'price' and 'contacts' sections' - 5 points
 
-Total score:  points
+Total score: 85/75 points
 
 developed by andrewkarev - 2022
 `;
