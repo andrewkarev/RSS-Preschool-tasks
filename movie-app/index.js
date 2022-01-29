9a535f0a5e4d42f71792596eecafeaeec8d0adff
andrewkarev <an.carev4@gmail.com>
2022-01-29 08:52:56 +0300

    feat: add variables

--- movie-app/index.js
+++ movie-app/index.js
@@ -1,1 +1,10 @@
 // Script
+const main = document.querySelector('.main');
+const form = document.querySelector('form');
+const search = form.querySelector('.header__search');
+
+const API_KEY = 'api_key=6f835e6610ba8bc0c78dd19839a3fe0c';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const START_PAGE_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
+
+console.log(START_PAGE_URL)
