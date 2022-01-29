9fa5cd86123d3fe7f5239ddb2e1bb27b70af71fb
andrewkarev <an.carev4@gmail.com>
2022-01-29 09:44:20 +0300

    feat: add func. for displaying  movies information

--- movie-app/index.js
+++ movie-app/index.js
@@ -6,10 +6,37 @@ const search = form.querySelector('.header__search');
 const API_KEY = 'api_key=6f835e6610ba8bc0c78dd19839a3fe0c';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const START_PAGE_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
 
 function getMovies(url) {
   fetch(url).then(response => response.json())
-    .then(x => console.log(x))
+    .then(function displayMovies(data) {
+      data.results.forEach(element => {
+        const item = document.createElement('div');
+        item.classList.add('main__item');
+        const image = document.createElement('img');
+        image.classList.add('main__img');
+        const about = document.createElement('div');
+        about.classList.add('main__about');
+        const title = document.createElement('h2');
+        const rating = document.createElement('div')
+        rating.classList.add('main__movie-rating');
+        const overview = document.createElement('div');
+        overview.classList.add('main__movie-overview');
+
+        image.src = IMG_PATH + element.poster_path;
+        title.textContent = element.title;
+        rating.textContent = element.vote_average;
+        overview.textContent = element.overview;
+
+        main.appendChild(item);
+        item.appendChild(image);
+        item.appendChild(about);
+        about.appendChild(title);
+        about.appendChild(rating);
+        about.appendChild(overview);
+      });
+    })
 }
 
 
