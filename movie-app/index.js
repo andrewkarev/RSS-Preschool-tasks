67ac5e8e1f07c295ee6e01db91d1c3a2e4a31207
andrewkarev <an.carev4@gmail.com>
2022-01-29 20:06:31 +0300

    feat: add genres list autocomplete with js

--- movie-app/index.js
+++ movie-app/index.js
@@ -2,12 +2,92 @@
 const main = document.querySelector('.main');
 const form = document.querySelector('.header__form');
 const search = form.querySelector('.header__search');
+const genreList = document.querySelector('.header__genre-list');
 
 const API_KEY = 'api_key=6f835e6610ba8bc0c78dd19839a3fe0c';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const START_PAGE_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = `${BASE_URL}/search/movie?${API_KEY}&query=`
+
+const genres = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 37,
+    name: "Western",
+  }
+];
 
 function getMovies(url) {
   fetch(url).then(response => response.json())
@@ -21,7 +101,7 @@ function getMovies(url) {
         about.classList.add('main__about');
         const title = document.createElement('h2');
         title.classList.add('main__movie-title');
-        const rating = document.createElement('div')
+        const rating = document.createElement('div');
         rating.classList.add('main__movie-rating');
         const overview = document.createElement('div');
         overview.classList.add('main__movie-overview');
@@ -54,6 +134,20 @@ function highlightRating(rate) {
     return 'bad';
   }
 }
+
+function addGenres() {
+  let names = genres.map(element => element.name);
+
+  names.forEach(name => {
+    const listitem = document.createElement('li');
+    listitem.classList.add('header__list-item');
+    listitem.textContent = name;
+
+    genreList.appendChild(listitem);
+  })
+}
+
+addGenres();
 
 function findMovie(e) {
   e.preventDefault();
